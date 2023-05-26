1b07801696dfe4a13942132617deb6e601fe6994
unknown <lakumkeval2004@gmail.com>
2023-05-26 14:47:40 +0530

    ajax

---
+++ index.js
@@ -0,0 +1,16 @@
+// promise
+
+// fetch("https://dummyjson.com/products")
+// .then((res)=>res.json())
+// .then((data)=>console.log(data))
+
+
+// async
+
+let getProducts=async() => {
+    let res = await fetch("https://dummyjson.com/products");
+    let products = await res.json();
+    console.log(products);
+}
+getProducts();
+
